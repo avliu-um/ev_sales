d3178a303da244bf1f19a1108934283e10632607
Alexander Liu <avliu@m-fvfg61a0q6lw.lan>
2023-05-23 12:52:31 -0400

    found item ID for every ebay sales link

--- get_ebay_sales_data.py
+++ get_ebay_sales_data.py
@@ -11,10 +11,6 @@ def get_sales_data(item_link):
     price = get_soup_text(soup, 'div[class*="vi-price"]', one=True)
     info['price'] = price
 
-    # TODO: Get the Item ID as a key
-    # TODO: Location
-    # TODO: Confirm sold
-
     labels_text = get_soup_text(soup, 'div[class*="labels-content"]')
     values_text = get_soup_text(soup, 'div[class*="values-content"]')
 
@@ -36,7 +32,10 @@ for sl in sales_links:
     print(f'\nsales link: {sl}')
     try:
         data_dict = get_sales_data(sl)
+
         data_dict['item_link'] = sl
+        data_dict['item_id'] = sl[len('https://www.ebay.com/itm/'):sl.find('?')]
+
         print(f'data: {data_dict}')
 
         append_to_json('data/ebay_ev_sales_data.json', data_dict)
