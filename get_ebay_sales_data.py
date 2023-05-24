c34d8d6a6f64185fe0b52b3bed8a4688923a4681
Alexander Liu <avliu@m-fvfg61a0q6lw.lan>
2023-05-24 12:21:08 -0400

    grabbing remaining links only when scraping, better printing for better debugging

--- get_ebay_sales_data.py
+++ get_ebay_sales_data.py
@@ -1,4 +1,6 @@
 import time, json
+import traceback
+
 import pandas as pd
 
 from util import get_soup, get_soup_text, append_to_json, remove_symbols_str
@@ -10,7 +12,9 @@ def get_sales_data(item_link):
 
     general_info = get_soup_text(soup, 'div[class="vi-cviprow"] div[class*="u-flL"]')
     for i in range(0, len(general_info), 2):
-        info[remove_symbols_str(general_info[i])] = general_info[i+1]
+        #print(f'i: {i}')
+        if i <= 0 and i+1 <= len(general_info):
+            info[remove_symbols_str(general_info[i])] = general_info[i+1]
 
     labels_text = get_soup_text(soup, 'div[class*="labels-content"]')
     values_text = get_soup_text(soup, 'div[class*="values-content"]')
@@ -24,16 +28,26 @@ def get_sales_data(item_link):
     return info
 
 
-sales_links = pd.read_csv('data/ebay_ev_sales_links.csv')['link']
+all_item_links = pd.read_csv('data/ebay_ev_sales_links.csv')['link']
+
+# Get remaining links to scrape
+with open('./data/ebay_ev_sales_data.json') as f:
+    data = json.load(f)
+    scraped_item_ids = list(map(lambda x: x['ebay_item_id'], data))
+    all_item_ids = all_item_links.apply(lambda x: x[len('https://www.ebay.com/itm/'):x.find('?')])
+
+    leftover_items = ~all_item_ids.isin(scraped_item_ids)
+    leftover_item_links = all_item_links[leftover_items]
+    leftover_item_links = leftover_item_links.unique()
 
 # testing
-sales_links = sales_links[1:10]
+#sales_links = sales_links[1:10]
 
-for sl in sales_links:
-    print(f'\nsales link: {sl}')
+for item_link in leftover_item_links:
+    print(f'\nitem link: {item_link}')
     try:
-        data_dict = get_sales_data(sl)
-        data_dict['ebay_item_id'] = sl[len('https://www.ebay.com/itm/'):sl.find('?')]
+        data_dict = get_sales_data(item_link)
+        data_dict['ebay_item_id'] = item_link[len('https://www.ebay.com/itm/'):item_link.find('?')]
 
         print(f'data: {data_dict}')
 
@@ -41,6 +55,7 @@ for sl in sales_links:
 
     except Exception as e:
         print(f'failed')
+        traceback.print_exc()
         print(e)
 
 print('\ndone!')
--- util.py
+++ util.py
@@ -1,10 +1,12 @@
 import json
 from os import path
 import re
+import time
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+import urllib.error
 
 
 def get_selenium_driver():
@@ -26,8 +28,26 @@ def get_selenium_driver():
 
 
 def get_soup(url):
-    req = Request(url)
-    html_page = urlopen(req)
+
+    success = False
+    sleep_time = 1
+    max_sleep_time = 60 * 5
+
+    req, html_page = None, None
+    while not success:
+        try:
+            req = Request(url)
+            html_page = urlopen(req)
+            success = True
+        except urllib.error.HTTPError as e:
+            print(f'error {e.code}')
+            if 500 <= e.code <= 599 and sleep_time < max_sleep_time:
+                print(f'server error; sleep {sleep_time} seconds')
+                time.sleep(sleep_time)
+                sleep_time *= 2
+            else:
+                raise e
+
     soup = BeautifulSoup(html_page, 'html.parser')
     return soup
 
