7ae1bf276a90cd03deb4ba6712c578db29b54280
Alexander Liu <avliu-um@github.com>
2023-08-03 10:25:34 -0400

    cloudwatch code

--- test_runner.py
+++ test_runner.py
@@ -1,5 +1,28 @@
 import boto3
+import datetime
+# TODO: Abstract these functions into util
 
+
+def get_log_time():
+    dt = datetime.datetime.now()
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    time = int((dt - epoch).total_seconds() * 1000.0)
+    return time
+
+def write_cloudwatch_log(message):
+    client = boto3.client('logs')
+    response = client.put_log_events(
+        logGroupName='test_local_to_cloudwatch',
+        logStreamName='test_stream',
+        logEvents=[
+            {
+                'timestamp': get_log_time(),
+                'message': message
+            },
+        ]
+        #,sequenceToken='string'
+    )
+    print(f'response: {response}')
 
 def write_to_bucket(aws_bucket, source, dest):
     # Make sure to configure ~/.aws/configure file
@@ -11,6 +34,7 @@ def test_write_s3():
     bucket = 'ev-cloud-testing'
     destination = 'test_copy/kbb_data.txt'
     write_to_bucket(bucket, "./kbb/data/data.json", destination)
+    write_cloudwatch_log('done!')
 
 
 if __name__ == '__main__':
