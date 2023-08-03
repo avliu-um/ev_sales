0467150432f0fbb00626d6450ffdeeb5d8338d68
Alexander Liu <avliu-um@github.com>
2023-08-03 09:48:24 -0400

    ready to test writing to s3

--- test_runner.py
+++ test_runner.py
@@ -1,3 +1,17 @@
+import boto3
 
-if __name__=='__main__':
-    print('hello world')
+
+def write_to_bucket(aws_bucket, source, dest):
+    # Make sure to configure ~/.aws/configure file
+    s3 = boto3.resource('s3')
+    s3.Bucket(aws_bucket).upload_file(source, dest)
+
+
+def test_write_s3():
+    bucket = 'ev-cloud-testing'
+    destination = 'test_copy/kbb_data.txt'
+    write_to_bucket(bucket, "./kbb/data/data.json", destination)
+
+
+if __name__ == '__main__':
+    test_write_s3()
