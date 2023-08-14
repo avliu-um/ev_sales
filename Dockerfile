d369675e7f1d1762c3763808cb5c920073a63f6b
Alexander Liu <avliu-um@github.com>
2023-08-13 23:12:50 -0400

    allow compatability with Chrome when running in container

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 # syntax=docker/dockerfile:1
 FROM python
-WORKDIR /code 
-RUN pip3 install --upgrade pip
-#RUN git clone https://github.com/avliu-um/asian_yelp.git
-COPY requirements.txt requirements.txt 
-RUN pip install -r requirements.txt
-
+WORKDIR /code
 COPY . .
+
+RUN pip3 install --upgrade pip
+RUN pip install -r requirements.txt
+# TODO: Test this
+RUN apt update && apt install -y chromium-driver
 
 ENTRYPOINT ["sh", "./run.sh"]
 
--- test_runner.py
+++ test_runner.py
@@ -1,7 +1,8 @@
 import boto3
 import datetime
 import os
-from scraper_util_avliu.util import get_soup, write_to_bucket
+from scraper_util_avliu.util import get_soup, write_to_bucket, get_selenium_driver
+import undetected_chromedriver as uc
 
 # TODO: Abstract these functions into util
 
@@ -12,6 +13,25 @@ def get_soup_write(url):
         file.write(str(soup.prettify()))
     write_to_bucket('ev-cloud-testing', './output.html',  'test_write_output.html')
 
+def get_driver_write(url):
+    # TODO: Update the get_driver function in util to allow driver path flexibility
+    chrome_options = uc.ChromeOptions()
+    chrome_options.add_argument('--mute-audio')
+    # chrome_options.add_extension(adblock_filepath)
+    driver = uc.Chrome(options=chrome_options,
+                       use_subprocess=True,
+                       headless=True,
+                       driver_executable_path='/usr/bin/chromedriver')
+
+    #driver = get_selenium_driver(undetected=True)
+    driver.get(url)
+    html = driver.page_source
+
+    with open("./output.html", "w", encoding='utf-8') as file:
+        # prettify the soup object and convert it into a string
+        file.write(html)
+    write_to_bucket('ev-cloud-testing', './output.html',  'test_write_output.html')
+
 
 if __name__ == '__main__':
     print(f'pwd: {os.getcwd()}')
@@ -20,4 +40,4 @@ if __name__ == '__main__':
            '%2Bm36IDbLHZAL4uxZXcIjHRoTmvK0QdYIILdy2AhV1CvRYpsdZg0'
            '%2FXnc2XZrwuw4J7YpWbHJiny14CAnLuEJPnuvvGZV6Hbh7ZJo5WucloyV0Hp3vQUGXNjxK129a9S%2FawYgT5'
            '%2Br0SR81vzzrrEVmXQDD60H4HqzPbhyenwROg9C5hIh3ThQtf01qzjbOvBQ%3D%3D%7Ctkp%3ABk9SR4KS4eGIYg')
-    get_soup_write(url)
+    get_driver_write(url)
